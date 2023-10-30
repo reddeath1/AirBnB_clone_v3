f5c675899d5a4cbcb755735c148bb2b359d9df34
ucheogbodo-25 <ucheogbodo25.com>
2023-10-30 15:54:14 -0700

    updated

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
+<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 """
 Created on Tue Sep  1 14:42:23 2020
 @author: Robinson Montes
          Mauricio Olarte
 """
+=======
+'''
+Creates a Blueprint instance with `url_prefix` set to `/api/v1`.
+'''
+
+
+>>>>>>> e0ff08e (updated)
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
+<<<<<<< HEAD
 
 if (__name__ == 'api.v1.views'):
     from api.v1.views.index import *
@@ -18,3 +27,13 @@ if (__name__ == 'api.v1.views'):
     from api.v1.views.users import *
     from api.v1.views.places import *
     from api.v1.views.places_reviews import *
+=======
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
+>>>>>>> e0ff08e (updated)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 """
 @authors: Frank Galos.
@@ -25,3 +26,38 @@ def object_status():
     for key, value in objects.items():
         objects[key] = storage.count(value)
     return jsonify(objects)
+=======
+'''
+Create a route `/status` on the object app_views.
+'''
+
+
+from flask import jsonify
+from api.v1.views import app_views
+from models import storage
+
+
+@app_views.route('/status', methods=['GET'])
+def api_status():
+    '''
+    Returns a JSON response for RESTful API health.
+    '''
+    response = {'status': 'OK'}
+    return jsonify(response)
+
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    '''
+    Retrieves the number of each objects by type.
+    '''
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stats)
+>>>>>>> e0ff08e (updated)
