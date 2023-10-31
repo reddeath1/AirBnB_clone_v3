96f7a0f8b04c2a3c5560590e722129897c1bb9f2
Reddeath <frankslayer1@gmail.com>
2023-11-01 00:40:39 +0300

    changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,30 +4,42 @@
 @authors: Frank Galos
           Uchechukwu Ogbodo
 """
-from os import getenv
-from flask import Flask, jsonify, Blueprint
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 from flask_cors import CORS
 
+
 app = Flask(__name__)
-CORS(app, origins="0.0.0.0")
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
+
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-CORS(app_views)
 
 
 @app.teardown_appcontext
-def close_db_sesion(error):
-    """ this for slash routing"""
+def tear(self):
+    ''' closes storage engine '''
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(e):
-    """handler for 404 errors that returns a JSON-formatted
-    404 status code response.
-    """
-    return ({'error': 'Not found'}), 404
+def not_found(error):
+    ''' handles 404 error and gives json formatted response '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == '__main__':
+    if getenv("HBNB_API_HOST") is None:
+        HBNB_API_HOST = '0.0.0.0'
+    else:
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    if getenv("HBNB_API_PORT") is None:
+        HBNB_API_PORT = 5000
+    else:
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
 
 
 if __name__ == "__main__":
